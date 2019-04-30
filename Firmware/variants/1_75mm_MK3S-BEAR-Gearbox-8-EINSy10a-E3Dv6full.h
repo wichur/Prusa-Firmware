8f9749aff415709c3fb4cf08fc29fd4672377ef8
vertigo235 <jbjonesclt@gmail.com>
2019-04-30 18:19:41 -0400

    Update Gearbox Configs

--- Firmware/variants/1_75mm_MK3S-BEAR-Gearbox-8-EINSy10a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MK3S-BEAR-Gearbox-8-EINSy10a-E3Dv6full.h
@@ -15,7 +15,7 @@
 #define DEVELOPER
 
 // Printer name
-#define CUSTOM_MENDEL_NAME "Bear MK3S GB16"
+#define CUSTOM_MENDEL_NAME "Bear MK3S GB8"
 
 // Electronics
 #define MOTHERBOARD BOARD_EINSY_1_0a
@@ -36,7 +36,7 @@
 
 // Steps per unit {X,Y,Z,E}
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,140}
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,980}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,490}
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,560}
 
 // Endstop inverting
@@ -202,7 +202,7 @@
 
 #define TMC2130_USTEPS_XY   16        // microstep resolution for XY axes
 #define TMC2130_USTEPS_Z    16        // microstep resolution for Z axis
-#define TMC2130_USTEPS_E    16        // microstep resolution for E axis
+#define TMC2130_USTEPS_E    8        // microstep resolution for E axis
 #define TMC2130_INTPOL_XY   1         // extrapolate 256 for XY axes
 #define TMC2130_INTPOL_Z    1         // extrapolate 256 for Z axis
 #define TMC2130_INTPOL_E    1         // extrapolate 256 for E axis
